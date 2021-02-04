eb2bdb93ebb0ae74cd47f7d2f82ab2c9d91f400a
BilalCHERFA <cherfabilal@gmail.com>
2021-02-04 23:32:27 +0100

    competences

--- src/components/achievements/Achievements.jsx
+++ src/components/achievements/Achievements.jsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import {BACK_URL, DOMAIN_URL} from '../../http';
 import Navigation from '../navigation/Navigation';
+import './achievements.css'
 
 const Achievements = () => {
+    const [achievements, setAchievements] = useState([]); 
+        
+    useEffect(() => {
+         const fetchAchievements = () => {
+            axios
+            .get(`${BACK_URL}/realisations`)
+            .then(res => setAchievements(res.data))
+         }
+         fetchAchievements()   
+     }, []);
+
     return (
-        <div>
+        <div className="section-achiev">
             <Navigation />
-            ici rélisations
+            <div className="achiev-container">
+                    {achievements.map((achiev) => (
+                        <div className="achiev" key={achiev.id}>
+                            <img className="project_image" src={`${DOMAIN_URL}/upload/${achiev.picture_url}`} alt={achiev.project_title}/>  
+                            <h4>{achiev.project_title}</h4>          
+                        </div>
+                    ))} 
+                </div>
         </div>
     );
 };
