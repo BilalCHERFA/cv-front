c214369a8eddeaba841d58e504380929f0c2c0db
BilalCHERFA <cherfabilal@gmail.com>
2021-02-05 01:03:00 +0100

    exp

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,12 +1,40 @@
-import React from 'react';
-import {BACK_URL, DOMAIN_URL} from '../../http';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';import {BACK_URL, DOMAIN_URL} from '../../http';
 import Navigation from '../navigation/Navigation';
+import './experience.css';
 
 const Experience = () => {
+    const [experience, setExperience] = useState([]); 
+        
+    useEffect(() => {
+         const fetchExp = () => {
+            axios
+            .get(`${BACK_URL}/experience`)
+            .then(res => setExperience(res.data))
+         }
+         fetchExp()   
+     }, []);
+
     return (
-        <div>
+        <div className="section-exp">
             <Navigation />
-            Expériences 
+            <div className="exp-container">
+                    {experience.map((exp) => (
+                        <div className="exp-map" key={exp.id}>
+                            <div className="exp-date">
+                                <h4>{exp.date_debut}</h4>
+                                <h4>{exp.date_fin}</h4> 
+                            </div>
+                            <div className="description-exp">
+                                <h3>{exp.position}</h3>
+                                <h4>{exp.company}</h4>
+                                <h5>- {exp.description_1}</h5>
+                                <h5>- {exp.description_2}</h5>
+                                <h5>- {exp.description_3}</h5>
+                            </div> 
+                        </div>
+                    ))} 
+                </div> 
         </div>
     );
 };
